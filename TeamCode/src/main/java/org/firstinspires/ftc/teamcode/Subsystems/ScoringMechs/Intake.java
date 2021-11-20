28650fef26528fde484244fd26b10edc503f05fd
Nathanael Gaw <nategaw@gmail.com>
2021-11-20 14:12:03 -0500

    Added the color sensor to intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ScoringMechs/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ScoringMechs/Intake.java
@@ -1,24 +1,40 @@
 package org.firstinspires.ftc.teamcode.Subsystems.ScoringMechs;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Subsystems.Subsytem;
+import org.firstinspires.ftc.teamcode.Subsystems.ValueTimer;
 
 public class Intake implements Subsytem {
 
     //Intake servos
     public CRServo intake;
 
+    //Sensors
+    public RevColorSensorV3 intakeColorSensor;
+
+    protected double intakeLength = 2;
+    ValueTimer<Double> colorDist;
+
     //State machine logic
     private double intakePower,lastIntakePower;
 
-    public Intake(HardwareMap hardwaremap, String intakeServoName, String wristServoName) {
+    public Intake(HardwareMap hardwaremap, String intakeServoName, String intakeColorSensorName) {
+        intakeColorSensor = hardwaremap.get(RevColorSensorV3.class, intakeColorSensorName);
         intake = hardwaremap.crservo.get(intakeServoName);
 
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        colorDist = new ValueTimer<Double>() {
+            @Override
+            public Double readValue() {
+                return intakeColorSensor.getDistance(DistanceUnit.INCH);
+            }
+        };
 
         intakePower = 0;
         lastIntakePower = 0;
@@ -35,6 +51,8 @@ public class Intake implements Subsytem {
 
     @Override
     public void update() {
+        colorDist.update();
+
         if (intakePower != lastIntakePower) {
             intake.setPower(intakePower);
         }
@@ -50,8 +68,13 @@ public class Intake implements Subsytem {
         this.intakePower = intakePower;
     }
 
+    public boolean getFreightInIntake () {
+        return colorDist.getValue() < intakeLength;
+    }
+
     @Override
     public void stop() {
+        colorDist.pause();
         setIntakePower(0);
         update();
     }
